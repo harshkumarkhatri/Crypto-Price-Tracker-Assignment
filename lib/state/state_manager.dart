2d7cdc7d7089bce07d3f83c9df02ca69cc8de8bd
Harsh <mailharshkhatri@gmail.com>
2023-03-20 15:14:37 +0530

    Updated state manager

--- lib/state/state_manager.dart
+++ lib/state/state_manager.dart
@@ -2,6 +2,9 @@ import 'package:cash_rich_assignment/model/coin.dart';
 import 'package:cash_rich_assignment/request/network_request.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final coinStateFuture = FutureProvider<Map<String, Coin>>((ref) async {
-  return getCoinData();
-});
+final coinStateFuture = FutureProvider.family<Map<String, Coin>, String>(
+  (ref, params) async {
+    final data = await getCoinData(params);
+    return data;
+  },
+);
