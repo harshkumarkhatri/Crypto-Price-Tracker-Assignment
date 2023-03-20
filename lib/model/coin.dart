e9d6a61fb92b92c53d3f77a6f2d7883388799ce2
Harsh <mailharshkhatri@gmail.com>
2023-03-20 15:13:28 +0530

    Updated coin model

--- lib/model/coin.dart
+++ lib/model/coin.dart
@@ -6,16 +6,20 @@ class Coin {
   String? name;
   String? symbol;
   int? cmcRank;
+  String? lastUpdated;
   Quote? quote;
+  List<dynamic>? tags;
 
-  Coin({id, name, symbol, cmcRank, quote});
+  Coin({id, name, symbol, cmcRank, lastUpdated, quote, tags});
 
   Coin.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
     symbol = json['symbol'];
     cmcRank = json['cmc_rank'];
+    lastUpdated = json['last_updated'];
     quote = json['quote'] != null ? Quote.fromJson(json['quote']) : null;
+    tags = json['tags'];
   }
 
   Map<String, dynamic> toJson() {
@@ -24,6 +28,8 @@ class Coin {
     data['name'] = name;
     data['symbol'] = symbol;
     data['cmc_rank'] = cmcRank;
+    data['last_updated'] = lastUpdated;
+    data['tags'] = tags;
     if (quote != null) {
       data['quote'] = quote!.toJson();
     }
